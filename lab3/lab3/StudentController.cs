f3d43923db1664602f74350cbeb9fed45432396d
Iulia Tamas <iuliatamas03@gmail.com>
2023-10-26 18:22:34 +0300

    .

---
+++ lab3/lab3/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lab3
+{
+    public class StudentController
+    {
+
+
+    }
+
+    public Student GetWithFilters(string name, int age) { 
+        return 
+
+    }
+
+    [HttpGet("fromRouteWithId/{id}")]
+    public Strudent GetById([FromRoute]int id) {
+        return StudentController.FirstOrDefault(string=> string.Id.Eqyals(id));
+
+    }
+
+    [HttpGet("fromHeader")]
+    public Strudent GetByIdwithFromHeader([FromRoute] int id)
+    {
+        return StudentController.FirstOrDefault(string=> string.Id.Eqyals(id));
+
+    }
+
+
+}
